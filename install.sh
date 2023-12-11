d291a46e96926d54c57175740f82be7c7df9c971
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-12-11 09:37:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+
+# Überprüfen, ob das Skript als Root ausgeführt wird
+if [ "$EUID" -ne 0 ]; then
+  echo "Bitte führen Sie das Skript als Root aus (mit sudo)."
+  exit 1
+fi
+
+# Überprüfen, ob Ubuntu Desktop installiert ist
+if [ -z "$(dpkg -l ubuntu-desktop 2>/dev/null | grep '^ii')" ]; then
+  echo "Ubuntu Desktop ist nicht installiert. Installiere es jetzt..."
+
+  # Aktualisiere die Paketliste
+  apt update
+
+  # Installiere Ubuntu Desktop
+  apt install ubuntu-desktop -y
+
+  echo "Ubuntu Desktop wurde erfolgreich installiert."
+else
+  echo "Ubuntu Desktop ist bereits installiert."
+fi
 
